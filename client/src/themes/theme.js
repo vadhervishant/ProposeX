f18fbd29c34b46d859df0b8d99ef91fd63e0c7d5
vadhervishant <vadhervishant@gmail.com>
2023-05-31 12:23:21 -0400

    Added Basic Architecture

---
+++ client/src/themes/theme.js
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material/styles';
+
+export const appTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#1d3a4d',
+    },
+    secondary: {
+      main: '#6a2153',
+    },
+    background: {
+      main: '#eaeaea',
+    },
+    surface: {
+      main: '#eaeaea',
+    },
+    success: {
+      main: '#15543c',
+    },
+    error: {
+      main: '#540000',
+    },
+  },
+});
