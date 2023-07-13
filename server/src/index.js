3dfadb7d133e19d44f5762fa3bcfc2f4349ac534
vadhervishant <vadhervishant@gmail.com>
2023-07-13 19:45:24 -0400

    Cleaned code for swagger

--- server/src/index.js
+++ server/src/index.js
@@ -3,32 +3,16 @@ const serverless = require('serverless-http');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 const userRoute = require('./routes/user');
+const { serveSwaggerUI, setupSwaggerUI } = require('./../utils/swagger');
 
 const jsonParser = bodyParser.json({ limit: '5mb' });
 app.use(cors());
 app.use(jsonParser);
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-      description: 'Documentation for the API endpoints',
-    },
-  },
-  apis: ['./src/routes/user.js'], // Specify the path to your route files
-};
-
-const swaggerSpec = swaggerJsdoc(options);
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/api-docs', swaggerUi.serveFiles(swaggerSpec), swaggerUi.setup(swaggerSpec));
 
 app.use('/api', userRoute);
+app.use('/api-docs', serveSwaggerUI, setupSwaggerUI);
 
 module.exports = app;
 module.exports.handler = serverless(app);
