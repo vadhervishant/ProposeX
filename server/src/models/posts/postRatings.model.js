5693f2dc03e1bbf0f8e482a608ba6d66add0a5cc
vadhervishant <vadhervishant@gmail.com>
2023-07-13 22:53:29 -0400

    Added Post Ratings model

---
+++ server/src/models/posts/postRatings.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('../../utils/dbConn');
+
+const postsRatingsModel = mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+    required: true,
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'posts',
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  dateCreated: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('postRatings', postsRatingsModel);
