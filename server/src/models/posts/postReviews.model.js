145565b4cb7fdc8d9ef1e3915ba15dd38378429d
vadhervishant <vadhervishant@gmail.com>
2023-07-13 22:53:15 -0400

    Added Post Reviews

---
+++ server/src/models/posts/postReviews.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('../../utils/dbConn');
+
+const postReviewsModel = mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+    required: true,
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'posts',
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  dateCreated: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('postReviews', postReviewsModel);
