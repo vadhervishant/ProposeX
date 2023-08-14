5c27bd570dc23411eafec35a0efb96c9a3de47cb
vadhervishant <vadhervishant@gmail.com>
2023-08-13 22:39:53 -0400

    Removed old files

--- server copy/index.js
+++
@@ -1,5 +0,0 @@
-const app = require('./src/index');
-
-app.listen(process.env.PORT || 5000, () => {
-  console.log('Server is up and running at ' + (process.env.PORT || 5000));
-});
--- server copy/utils/throwError.js
+++
@@ -1,9 +0,0 @@
-const throwError = (status, message) => {
-    var err = new Error();
-    err.status = status || 500;
-    err.message = message || 'Internal Server Error';
-    return err;
-  };
-  
-  module.exports = throwError;
-  
